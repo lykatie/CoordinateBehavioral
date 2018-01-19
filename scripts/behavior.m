5ce8b545fea7679b7420effedcd84601587a82ff
James Wu <jiwu@cs.washington.edu>
2018-01-19 12:19:50 -0800

    add trial counter

--- scripts/behavior.m
+++ scripts/behavior.m
@@ -1,4 +1,4 @@
-function [ position, controlVel, errorStruct, rotation ] = behavior( behaviorcells )
+function [ position, controlVel, controlPol, rotation, runnum ] = behavior( behaviorcells )
 %BEHAVIOR Summary of this function goes here
 %   Detailed explanation goes here
 %COMPUTE_BUTNALIGN Summary of this function goes here
@@ -14,16 +14,30 @@ function [ position, controlVel, errorStruct, rotation ] = behavior( behaviorcel
     controlVel = cell2mat(behaviorcells(:, col.vel));
 
     rotationraw = unity_struct.events(:, 1);
-    rotationraw(rotationraw >= 2000 | rotationraw < 1000) = 0;
-    rotationraw = rotationraw - 1000;
-    rotationraw(1) = 0;
+    
+    rotation = rotationraw;
+    rotation(rotation >= 2000 | rotation < 1000) = 0;
+    rotation = rotation - 1000;
+    rotation(1) = 0;
+    
+    runnum = zeros(size(rotationraw));
+    runnum(1) = 1;
+
     for i = 2:length(rotationraw)
-        if(rotationraw(i) == -1000)
-            rotationraw(i) = rotationraw(i-1);
+        if(rotation(i) == -1000)
+            rotation(i) = rotation(i-1);
+        end
+        if(rotationraw(i) == 2000)
+            runnum(i) = runnum(i-1)+1;
+        else
+            runnum(i) = runnum(i-1);
         end
     end
-    rotation = rotationraw;
 
-    errorStruct = struct();
+    controlPol = nan(size(controlVel));
+    [controlPol(:, 1), controlPol(:, 2)] = ...
+        cart2pol(controlVel(:, 1), controlVel(:, 2));
+    
+    
 end
 
