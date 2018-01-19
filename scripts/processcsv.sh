7d6f6cdf75eacb066334a8b679264349677b5cd6
James Wu <jiwu@cs.washington.edu>
2018-01-19 01:10:18 -0800

    modify for batch processing

--- scripts/processcsv.sh
+++ scripts/processcsv.sh
@@ -6,17 +6,22 @@
 #      look at the next line; if next does start with "!"
 #      print the next line on same line as current line
 #      else print them separately
-
-tail -n +3 $1 | \
-sed 's/[()]//g' | \
-sed 's/, /,/g' | \
-awk '!/^!/ { \
-        if((getline n) > 0) { \
-            if (n ~ /^!/) { \
-                if (NR == 1) \
-                    printf("%s%s\n%s",$0,"!Reset",n); \
-                printf("\n%s%s",$0,n); } \
-            else { printf("\n%s\n%s",$0,n); } } \
-        else \
-            { printf("\n%s\n",$0); } } \
-    /^!/ { printf("%s",$0) }'
+for filename in ../data_raw/*.csv; do
+    echo -n "Processing "
+    echo $(basename $filename)
+    echo "time,x,z,y,jx,jy,wx,wy,wz,event" > ../data_processed/$(basename $filename)
+    tail -n +3 $filename | \
+    sed 's/[()]//g' | \
+    sed 's/, /,/g' | \
+    gawk '!/^!/ { \
+            if((getline n) > 0) { \
+                if (n ~ /^!/) { \
+                    if (NR == 1) \
+                        printf("%s%s\n%s",$0,"!Reset",n); \
+                    printf("\n%s%s",$0,n); } \
+                else { printf("\n%s\n%s",$0,n); } } \
+            else \
+                { printf("\n%s\n",$0); } } \
+                    /^!/ { printf("%s",$0) }' | \
+    tail -n +2 >> ../data_processed/$(basename $filename)
+done
