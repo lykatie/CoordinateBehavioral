0fceef95b6bcefcb40c9e4b3bb2264a7ff7aae86
James Wu <jiwu@cs.washington.edu>
2018-03-14 13:33:13 -0700

    pass trial error mean and variance

--- scripts/summaryerror.m
+++ scripts/summaryerror.m
@@ -1,4 +1,4 @@
-function [improvement, improvementnorm, runerror] = summaryerror(error, rotation, runnum)
+function [improvement, improvementnorm, runerror, meanerror, sderror] = summaryerror(error, rotation, runnum)
 %SUMMARYERROR Summary of this function goes here
 %   Detailed explanation goes here
     runlist = unique(runnum);
@@ -11,9 +11,13 @@ function [improvement, improvementnorm, runerror] = summaryerror(error, rotation
     rotlist = [0 45 90 180];
     improvement = nan(numel(rotlist), 1);
     improvementnorm = improvement;
+    meanerror = improvement;
+    sderror = improvement;
     for i = 1:numel(rotlist)
         rot_oi = rotlist(i);
         run_oi = find(runerror(:, 2) == rot_oi);
+        meanerror(i) = mean(runerror(run_oi, 1));
+        sderror(i) = std(runerror(run_oi, 1)/sqrt(numel(run_oi)));
         coeffs = polyfit((1:length(run_oi))', runerror(run_oi, 1), 1);
         improvement(i) = coeffs(1);
         improvementnorm(i) = coeffs(1) / coeffs(2);
